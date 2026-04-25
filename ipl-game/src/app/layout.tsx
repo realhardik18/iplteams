863bdd6c88ceee953e53a3d796f284f7b4dcffb5
ajeebtech <jatintheboy@gmail.com>
2026-04-25 23:33:05 +0530

    v

--- ipl-game/src/app/layout.tsx
+++ ipl-game/src/app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "IPL Guessing Game",
+  title: "Guess the Player",
   description: "Can you guess the IPL player based on the teams they've played for?",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
